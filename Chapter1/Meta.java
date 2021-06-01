686fdd1b699311f399baa97938b79fd8d3f17a66
mchoe89 <75789599+mchoe89@users.noreply.github.com>
2021-06-01 09:17:42 -0700

    Add files via upload

---
+++ Chapter1/Meta.java
@@ -0,0 +1,12 @@
+// Write a complete Java program called Meta whose output is the text that would be the source code of a Java program
+// that prints “Hello, world!” as its output.
+
+public class Meta {
+    public static void main(String[] args) {
+        System.out.println("public class Hello {");
+        System.out.println("    public static void main(String[] args) {");
+        System.out.println("        System.out.println(\"Hello, world!\");");
+        System.out.println("    }");
+        System.out.println("}");
+    }
+}
